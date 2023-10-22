0e08dbc7f5c2d5ae47b0d461bf418db32ea971cf
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2023-10-23 00:59:27 +0600

    First

--- app/src/main/java/com/example/ceilingontop/ActivityLoginUser.java
+++ app/src/main/java/com/example/ceilingontop/ActivityLoginUser.java
@@ -1,14 +1,73 @@
 package com.example.ceilingontop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ActivityLoginUser extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText email, password;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
+
+        auth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email_et);
+        password = findViewById(R.id.password_et);
+        button = findViewById(R.id.btn_sign_in);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = email.getText().toString();
+                String pass = password.getText().toString();
+                if(!user.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(user).matches()){
+                    if(!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(user, pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast toast = Toast.makeText(ActivityLoginUser.this, "Login Successful", Toast.LENGTH_SHORT);
+                                toast.show();
+                                startActivity(new Intent(ActivityLoginUser.this, MainActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast toast = Toast.makeText(ActivityLoginUser.this, "Login Failed", Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        });
+                        }
+                    else {
+                        password.setError("Please enter password");
+                    }
+                    }
+                else if(user.isEmpty()){
+                    email.setError("Please enter email id");
+                }
+                else {
+                    email.setError("Please enter valid email id");
+                }
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/ceilingontop/ActivityRegistrationUser.java
+++ app/src/main/java/com/example/ceilingontop/ActivityRegistrationUser.java
@@ -1,14 +1,73 @@
 package com.example.ceilingontop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ActivityRegistrationUser extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText email, password, name, phone;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_user);
+
+
+        auth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.id_email_newuser);
+        password = findViewById(R.id.id_password_newuser);
+        name = findViewById(R.id.id_fullname_newuser);
+        phone = findViewById(R.id.id_mobileno_newuser);
+        button = findViewById(R.id.btn_regitser);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = email.getText().toString();
+                String pass = password.getText().toString();
+                if (user.isEmpty()){
+                    email.setError("Please enter email id");
+                }
+                if (pass.isEmpty()){
+                    password.setError("Please enter password");
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                startActivity(new Intent(ActivityRegistrationUser.this, ActivityLoginUser.class));
+                            }
+                            else {
+                                Toast toast = Toast.makeText(ActivityRegistrationUser.this, "Registration Failed", Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+
+
+
+
+
     }
 }
